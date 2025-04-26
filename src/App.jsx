ca755b9965a753d258a46d0de824e7cc3c647829
Umesh gupta <umeshg2059@gmail.com>
2025-04-26 09:28:14 +0530

    recoil

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,43 @@ import {useContext,useState} from "react"
 import {countContext} from "./context"
 
 function App(){
-  const [count,setCount]=useState(0);
  return(
   <div>
-    <countContext.provider value={count}>
-       <count setCount={setCount}/>
-       </countContext.provider>
+    <RecoilRoot>
+    <count/>
+    </RecoilRoot>
   </div> 
  )
 }
 
-function Count({setCount}){
+function Count(){
   return(
     <div>
       <CountRenderer/>
-      <Buttons setCount={setCount}/>
+      <Buttons/>
     </div>
   )
+}
+
+function CountRenderer(){
+  const count=useRecoilValue(countAtom)
+  return (
+    <div>
+      <b>
+          {count}
+      </b>
+    </div>
+  )
+}
+
+function Buttons(){
+  const [count,setCount]=useRecoilState(countAtom);
+
+  return <div>
+    <button onclick={()=>{
+      setCount(count + 1)}}>increase</button>
+
+    <button onclick={()=>{
+      setCount(count - 1)}}>decrease</button>
+  </div>
 }
